2f0f4117b7337a6719e0a7aa43894044ea908155
jorge mario herrera vargas <jorgemario2pac@gmail.com>
2021-01-18 14:41:02 -0500

    Formularrio validado por template, se realizan varias formas de validacion de template

--- formularios/src/app/app-routing.module.ts
+++ formularios/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { ReactiveComponent } from './componentes/reactive/reactive.component';
+import { TemplateComponent } from './componentes/template/template.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'template', component: TemplateComponent},
+  {path: 'reactivo', component: ReactiveComponent},
+  {path: '**', pathMatch: 'full',  redirectTo: 'template'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ formularios/src/app/componentes/template/template.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PaicesService } from 'src/app/servicios/paices.service';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  usuario = {
+    nombre: 'mario',
+    apellido: 'herrera',
+    email: 'correo@corre.com',
+    pais: ''
+  };
+
+  paises: any[] = [];
+
+  constructor(private pais: PaicesService) { }
+
+  ngOnInit(): void {
+    this.pais.obtenerPaices().subscribe(paices => {
+      this.paises = paices;
+      this.paises.unshift({
+        nombre: 'Seleccione un País.',
+        codigo: ''
+      });
+    });
+  }
+
+  guardar(form: NgForm) {
+    if (form.invalid) {
+      Object.values(form.controls).forEach( control => {
+        control.markAsTouched();
+      });
+      return;
+    }
+    console.log(form);
+    console.log(form.value);
+  }
+
+}
